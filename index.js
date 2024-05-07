2bb137fc53a85ed48c4f4e73a22a7fad7673139f
Mwaniki Tifany <111235348+RivoltaAlpha@users.noreply.github.com>
2024-05-07 15:56:15 +0300

    Create index.js
    Reaction functions

---
+++ index.js
@@ -0,0 +1,25 @@
+function showReaction() {
+    var anime = document.getElementById('animeInput').value.trim();
+
+    if (anime !== '') {
+        var reaction = getReaction(anime);
+        alert('Your favorite anime is ' + anime + '! Reaction: ' + reaction);
+    } else {
+        alert('Please enter your favorite anime!');
+    }
+}
+
+function getReaction(anime) {
+    switch (anime.toLowerCase()) {
+        case 'naruto':
+            return 'Believe it!';
+        case 'one piece':
+            return 'I am going to be the Pirate King!';
+        case 'attack on titan':
+            return 'Erwin Smith: Dedicate your hearts!';
+        case 'my hero academia':
+            return 'Plus Ultra!';
+        default:
+            return 'Excelsior!';
+    }
+}
